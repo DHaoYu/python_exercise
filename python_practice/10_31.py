be1f9b842bf3502fa4160b2583c43b63dbda222c
DHaoYu <178070667@qq.com>
2019-11-03 23:22:06 +0800

    update

--- python_practice/10_31.py
+++ python_practice/10_31.py
@@ -24,7 +24,46 @@ exists('d:/aaa.txt') #可以直接使用
 # 耦合：两者之间关联关系程度
 '''
 
-import sys 
-import sys #导入第二遍时，会被忽略 只执行一遍 执行这个过程视为：加载 
+#import sys 
+#import sys #导入第二遍时，会被忽略 只执行一遍 执行这个过程视为：加载 
 #会生成一个__pycache__的文件，会包含导入的模块（二进制）
 #python 中有一中操作，先导入一个模块，在程序运行过程中，把模块修改，如果想让修改生效，可以使用relode重新加载
+
+'''
+def multi_sum(*args):
+    s = 0
+    for item in args:
+        s += item
+    return s
+
+print(multi_sum(1, 2, 3, 4))  # 通常*之后的内容是一个元组
+
+#print(multi_sum(1, 2, 3, '1')) error
+a = (1, 2, 'a', 'b')
+print(a)
+'''
+'''
+def do_something(name, age, gender = "man", *args, **kwds): #两个**表示传递的是一个字典（实参需要用=之间关联）
+    print('name: %s, age: %d, gender: %s' %(name, age, gender))
+    print('high: %d, weight: %d' %(args[0], args[1]))
+    print(kwds)
+
+do_something('zhangsan', 18, 'man', 180, 60, math=99, english=100)
+'''
+'''
+y = 5
+print('y is a -' if y < 0 else 'y is a +') #可以直接在print函数中使用
+x = -1 if y < 0 else 1 #可以用于赋值
+print(x)
+
+a = [1, 2, 3, 4, 5]
+result = [i*i for i in a]
+print(result)
+'''
+
+a = [1, 2, 3, 4, 5]
+b = ['a', 'b', 'c', 'd']
+#a[2:2] = b
+#print(a)
+a[3:] = b
+print(a)
