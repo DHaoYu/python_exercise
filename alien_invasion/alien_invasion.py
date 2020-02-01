0914bdcec9c0c53203d7dfce32e5bea776950d3d
DHaoYu <178070667@qq.com>
2020-02-01 22:14:31 +0800

    alien_invasion

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,33 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+from pygame.sprite import Group
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #bg_color = (230, 230, 230)
+    #创建飞船对象
+    ship = Ship(ai_settings, screen)
+    #创建外星人对象编组（多个外星人）
+    aliens = Group()
+    #创建一个用于存储子弹的编组
+    bullets = Group()
+    gf.create_fleet(ai_settings, screen, aliens)
+
+    #开始游戏主循环
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_aliens(aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+#help(pygame)
+run_game()
