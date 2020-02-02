4fcd4d62859f0bd1f45e2eacad77b267d69c85bf
DHaoYu <178070667@qq.com>
2020-02-02 21:42:36 +0800

    alien

---
+++ alien_invasion/alien.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """表示单个外星人的类"""
+    def __init__(self, ai_settings, screen):
+        super().__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        #加载外星人图像
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        #将每个外星人绘制在屏幕左上角
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        #存储外星人的准确位置
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        """在指定位置绘制外星人"""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """更新外星人位置"""
+        self.x += self.ai_settings.alien_speed_factor*self.ai_settings.fleet_direction
+        self.rect.x = self.x
+
+    def change_direction(self):
+        self.ai_settings.fleet_direction *= -1
+
+    def check_edges(self):
+        """检查外星人是否移动到边缘"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return 1
+        elif self.rect.left <= 0:
+            return 1
