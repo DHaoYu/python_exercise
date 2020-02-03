c969506555f1f0c6a364bed24fddb39b53448559
DHaoYu <178070667@qq.com>
2020-02-03 22:38:37 +0800

    update

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from alien import Alien
+from game_stats import GameStats
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -11,7 +12,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+ 
     #bg_color = (230, 230, 230)
     #创建飞船对象
     ship = Ship(ai_settings, screen)
@@ -20,15 +21,17 @@ def run_game():
     #创建一个用于存储子弹的编组
     bullets = Group()
     gf.create_fleet(ai_settings, screen, aliens)
+    #创建游戏信息对象
+    stats = GameStats(ai_settings)
 
     #开始游戏主循环
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-        gf.check_hit(aliens, bullets)
-        gf.update_aliens(ai_settings, aliens)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-
+        if stats.game_active:
+            gf.check_events(ai_settings, screen, ship, bullets)
+            ship.update()
+            bullets.update()
+            gf.check_hit(ai_settings, screen, aliens, bullets)
+            gf.update_aliens(ai_settings, ship, aliens, stats, bullets, screen)
+            gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 #help(pygame)
 run_game()
